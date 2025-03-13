390dfee9713bcb330e2d30bf7f5b3643d3ece217
azizca7 <ikromovazizbek256@gmail.com>
2025-03-13 10:02:56 +0500

    update

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as Navbar } from './Navbar';
+export { default as About } from './About';
+export { default as Blog } from './Blog';
+export { default as Contact } from './Contact';
+export { default as Demo } from './Demo';
+export { default as Footer } from './Footer';
+export { default as Hero } from './Hero';
+export { default as Info } from './Info';
+export { default as Stats } from './Stats';
+export { default as Tasks } from './Tasks';
