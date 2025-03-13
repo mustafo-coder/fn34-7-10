94f6103c0052b83957ae6d654f0545f2b2987830
itsforme <sultanovabdulxamid@gmail.com>
2025-03-13 10:31:15 +0500

    qwe

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-	return <div></div>;
+  return (
+    <>
+      <div className="w-full container my-20 mx-40 px-16 py-14 bg-[#E6F7FF] flex flex-col gap-3">
+        <div className="flex flex-col items-start justify-center gap-[18px] max-w-[495px]">
+          <h2 className="font-franc text-5xl text-[#1F1F1F]">
+            Get started with Whirl
+          </h2>
+          <p className="font-inter text-[#1F1F1F] text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt.
+          </p>
+          <button className="mt-[18px] bg-[#0070A0] text-white font-inter px-8 py-2 text-center">
+            Book a demo
+          </button>
+        </div>
+        <div className="flex items-center justify-between w-[30%]">
+          <div>
+            <i className="fa-solid fa-check text-[#626A72]"></i>
+            <a href="#" className="font-inter text-sm text-[#626A72]">
+              Free 30-day trial
+            </a>
+          </div>
+          <div>
+            <i className="fa-solid fa-check text-[#626A72]"></i>
+            <a href="#" className="font-inter text-sm text-[#626A72]">
+              No credit-card required
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
